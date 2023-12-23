a41039901596b1c609a017fb05d71ff4c356a00b
tayyibgondal <tayyibgondal2003@gmail.com>
2023-12-23 19:40:06 +0500

    Change port

--- imageRoutes.js
+++ imageRoutes.js
@@ -99,7 +99,6 @@ router.delete('/remove/:imageId', async (req, res) => {
 router.get('/images/:userId', async (req, res) => {
     try {
         const userId = req.params.userId;
-        
 
         // Fetch images for the specified user
         const images = await ImageModel.find({ userId: userId });
