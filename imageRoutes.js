769b8762fceff357259a0222727ae77a7c2c5375
SyedAbdulrab <syedabdulrab1133@gmail.com>
2023-12-24 14:19:51 +0500

    add quota to image delete route

--- imageRoutes.js
+++ imageRoutes.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const ImageModel = require("./models/ImageModel");
-const { createLogEntry } = require('./loggingService');
+const { createLogEntry } = require("./loggingService");
 
 const router = express.Router();
 const port = 3001;
@@ -32,132 +32,154 @@ router.get("/upload", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-
 // Your existing image service route
 
-const axios = require('axios');
+const axios = require("axios");
 
-router.post('/upload', upload.single('image'), async (req, res) => {
-    try {
-        
-      console.log("ALMOSTTTTTT TEHERSSS");
-  
-      const userId = req.body.userId;
-      console.log("IMAGE SIZEEEEEEEEEEEE ", req.file.size);
-  
-      // Check quotas with the auth service
-      console.log(`http://localhost:3003/updateQuotas/${userId}`)
-  
-      const quotasCheckResponse = await axios.post(`http://localhost:3003/updateQuotas/${userId}`, {
+router.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    console.log("ALMOSTTTTTT TEHERSSS");
+
+    const userId = req.body.userId;
+    console.log("IMAGE SIZEEEEEEEEEEEE ", req.file.size);
+
+    // Check quotas with the auth service
+    console.log(`http://localhost:3003/updateQuotas/${userId}`);
+
+    const quotasCheckResponse = await axios.post(
+      `http://localhost:3003/updateQuotas/${userId}`,
+      {
         amount: req.file.size,
-        type: 'upload',
-      });
-
-      console.log("HERE IS TEH RESPONSE DATA MESSAGE")
-      console.log(quotasCheckResponse);
-  
-      if (quotasCheckResponse.status !== 200) {
-        // Check if the quota has been exceeded
-        console.log(quotasCheckResponse.status,"AHHAHAHHAHAHAH");
-
-        if (quotasCheckResponse.status === 205) {
-          return res.status(400).json({ message: 'Sorry, your quota for the day has exceeded!' });
-        } else {
-          // For other errors, return a generic error message
-          return res.status(500).json({ message: 'Internal server error BHAI YEH KYA HORAHA HAI' });
-        }
+        type: "upload",
       }
-  
-      // Quota check passed, proceed with image upload
-      const result = await cloudinary.uploader.upload(req.file.path);
-      const newImage = new ImageModel({
-          public_id:result.public_id,
-        userId: userId,
-        imageUrl: result.secure_url,
-        imageSize: req.file.size,
-      });
-  
-      // Save the image to the database
-      await newImage.save();
-  
-      console.log('IMAGE UPLOAD SUCCESSFUL. url->', result.secure_url);
-  
-      // Create a log entry
-      await createLogEntry(userId, 'Image Upload', req.file.size);
-  
-      res.status(200).json({
-        url: result.secure_url,
-        size: req.file.size,
-        userId: userId,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('An error occurred');
-    }
-  });
-  
-  
-  
+    );
 
+    console.log("HERE IS TEH RESPONSE DATA MESSAGE");
+    console.log(quotasCheckResponse);
 
-router.delete('/remove/:imageId/:publicId', async (req, res) => {
-    try {
-      const imageId = req.params.imageId;
-      const publicId = req.params.publicId;
-  
-      // Find the image in the database
-      const image = await ImageModel.findById(imageId);
-    console.log("Image ",image);
-      if (!image) {
-        return res.status(404).json({ message: 'Image not found' });
+    if (quotasCheckResponse.status !== 200) {
+      // Check if the quota has been exceeded
+
+      if (quotasCheckResponse.status === 205) {
+        return res
+          .status(400)
+          .json({ message: "Sorry, your quota for the day has exceeded!" });
+      } else {
+        // For other errors, return a generic error message
+        return res
+          .status(500)
+          .json({ message: "Internal server error BHAI YEH KYA HORAHA HAI" });
       }
-  
-      // Remove the image from Cloudinary
-      
-      await cloudinary.uploader.destroy(publicId);
-      console.log("destroyingggg, public ID",publicId);
-  
-      // Remove the image from the database
-      await ImageModel.findByIdAndDelete(imageId);
-  
-      // Create a log entry for image removal
-      await createLogEntry(image.userId, 'Image Removal', image.imageSize);
-  
-      res.status(200).json({ message: 'Image removed successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('An error occurred');
     }
-  });
-  
 
+    // Quota check passed, proceed with image upload
+    const result = await cloudinary.uploader.upload(req.file.path);
+    const newImage = new ImageModel({
+      public_id: result.public_id,
+      userId: userId,
+      imageUrl: result.secure_url,
+      imageSize: req.file.size,
+    });
+
+    // Save the image to the database
+    await newImage.save();
+
+    console.log("IMAGE UPLOAD SUCCESSFUL. url->", result.secure_url);
+
+    // Create a log entry
+    await createLogEntry(userId, "Image Upload", req.file.size);
+
+    res.status(200).json({
+      url: result.secure_url,
+      size: req.file.size,
+      userId: userId,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+});
+
+router.delete("/remove/:imageId/:publicId", async (req, res) => {
+  try {
+    const imageId = req.params.imageId;
+    const publicId = req.params.publicId;
+
+    const quotasCheckResponse = await axios.post(
+      `http://localhost:3003/updateQuotas/${userId}`,
+      {
+        amount: req.file.size,
+        type: "delete",
+      }
+    );
+
+    console.log("HERE IS TEH RESPONSE DATA MESSAGE");
+    console.log(quotasCheckResponse);
+
+    if (quotasCheckResponse.status !== 200) {
+      // Check if the quota has been exceeded
+
+      if (quotasCheckResponse.status === 205) {
+        return res
+          .status(400)
+          .json({ message: "Sorry, your quota for the day has exceeded!" });
+      } else {
+        // For other errors, return a generic error message
+        return res
+          .status(500)
+          .json({ message: "Internal server error BHAI YEH KYA HORAHA HAI" });
+      }
+    }
+
+    // Find the image in the database
+    const image = await ImageModel.findById(imageId);
+    console.log("Image ", image);
+    if (!image) {
+      return res.status(404).json({ message: "Image not found" });
+    }
+
+    // Remove the image from Cloudinary
+
+    await cloudinary.uploader.destroy(publicId);
+    console.log("destroyingggg, public ID", publicId);
+
+    // Remove the image from the database
+    await ImageModel.findByIdAndDelete(imageId);
+
+    // Create a log entry for image removal
+    await createLogEntry(image.userId, "Image Removal", image.imageSize);
+
+    res.status(200).json({ message: "Image removed successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+});
 
 // Route to fetch images for a particular userId
-router.get('/images/:userId', async (req, res) => {
-    try {
-        const userId = req.params.userId;
+router.get("/images/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
 
-        // Fetch images for the specified user
-        const images = await ImageModel.find({ userId: userId });
-        res.status(200).json(images);
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
+    // Fetch images for the specified user
+    const images = await ImageModel.find({ userId: userId });
+    res.status(200).json(images);
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 // Route to fetch all images
-router.get('/images', async (req, res) => {
-    try {
-        // Fetch all images
-        const images = await ImageModel.find();
-        res.status(200).json(images);
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
+router.get("/images", async (req, res) => {
+  try {
+    // Fetch all images
+    const images = await ImageModel.find();
+    res.status(200).json(images);
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
-
-
 
 module.exports = router;
